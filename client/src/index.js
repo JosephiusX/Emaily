4118bdc831972ea6bb9d04cf1e41bbf8df612839
josephius x <josephiusx@gmail.com>
2023-03-04 11:10:22 -0800

    #90. Additional Proxy Rules

--- client/src/index.js
+++ client/src/index.js
@@ -3,11 +3,13 @@ import React from 'react';
 import ReactDOM from 'react-dom'
 import { Provider } from 'react-redux';
 import { createStore, applyMiddleware } from 'redux';
+import reduxThunk from 'redux-thunk';
 
 import App from './components/App'
 import reducers from './reducers';
 
-const store = createStore(reducers, {}, applyMiddleware());// 1st arg-reducers(=> fn returning an array),2nd Arg-initial state(empty object), 3rd- applyMiddleware
+
+const store = createStore(reducers, {}, applyMiddleware(reduxThunk));// 1st arg-reducers(=> fn returning an array),2nd Arg-initial state(empty object), 3rd- applyMiddleware
 
 ReactDOM.render(
 <Provider store={store}><App /></Provider>, // Provider reads changes from our redux store. 
